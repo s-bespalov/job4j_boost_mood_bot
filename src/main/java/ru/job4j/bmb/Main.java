5301293f0086eeed3b2547f5555a8a2753c4ceb2
Sergey Bespalov <sergey.bespalov@protonmail.com>
2024-10-31 23:12:04 +0300

    Тест

--- src/main/java/ru/job4j/bmb/Main.java
+++ src/main/java/ru/job4j/bmb/Main.java
@@ -2,6 +2,6 @@ package ru.job4j.bmb;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет. Мир");
+        System.out.println("Привет. Мир 2");
     }
 }
