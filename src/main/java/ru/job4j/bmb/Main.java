81a43d75968a54b02a138dfe103fe633f7abbd05
Sergey Bespalov <sergey.bespalov@protonmail.com>
2024-11-02 19:54:51 +0300

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/bmb/Main.java
+++ src/main/java/ru/job4j/bmb/Main.java
@@ -1,7 +1,23 @@
 package ru.job4j.bmb;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.services.TelegramBotService;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет. Мир 2");
+    }
+
+    @Bean
+    public CommandLineRunner initTelegramApi(ApplicationContext ctx) {
+        return args -> {
+            var bot = ctx.getBean(TelegramBotService.class);
+            bot.receive(new Content());
+        };
     }
 }
--- src/main/java/ru/job4j/bmb/Repositories.java
+++ src/main/java/ru/job4j/bmb/Repositories.java
@@ -1,4 +1,7 @@
 package ru.job4j.bmb;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class Repositories {
 }
